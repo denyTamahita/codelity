f4d564be419be81157902aade33337022474ad5b
deny.baskoro <deny.baskoro@collega.co.id>
2018-08-29 15:06:02 +0700

    version commit

--- src/Codelity.java
+++ src/Codelity.java
@@ -1,16 +1,25 @@
-import binarygap.Solution;
-
 /**
  * Created by user on 8/29/2018.
  */
 public class Codelity {
-    public static void main(String[] args) {
 
-        Solution sol = new Solution();
+    public static void main(String[] args) {
+        CyclicRotation.Solution sol = new CyclicRotation.Solution();
+        int[] numbers = new int[]{1,2,3,4};
+        numbers = sol.solution(numbers,4);
+        for (int i = 0; i < numbers.length ; i++) {
+            System.out.print(numbers[i]);
+        }
+    }
+
+
+    public static void mainBinaryGap(String[] args) {
+
+        binarygap.Solution sol = new binarygap.Solution();
         int n = 20;
         System.out.println(sol.solution(n));
-        }
-
-
-
     }
+
+}
+
+
