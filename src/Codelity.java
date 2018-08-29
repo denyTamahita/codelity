aac69a4d88c577271eb0bb101e41ee51307138be
deny.baskoro <deny.baskoro@collega.co.id>
2018-08-29 12:06:16 +0700

    Initial commit

---
+++ src/Codelity.java
@@ -0,0 +1,14 @@
+/**
+ * Created by user on 8/29/2018.
+ */
+public class Codelity {
+    public static void main(String[] args) {
+
+        Solution sol = new Solution();
+        int n = 20;
+        System.out.println(sol.solution(n));
+        }
+
+
+
+    }
